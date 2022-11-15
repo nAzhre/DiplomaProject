2d6cfdee26ec3615029d4cfd26608b3469da2865
Yerzhan <erzh896@gmail.com>
2022-11-15 23:55:27 +0600

    try 1

---
+++ src/main/java/kz/iitu/diploma/diplomaproject/DiplomaProjectApplication.java
@@ -0,0 +1,14 @@
+package kz.iitu.diploma.diplomaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaProjectApplication.class, args);
+    }
+
+}
+//add
---
+++ src/test/java/kz/iitu/diploma/diplomaproject/DiplomaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.iitu.diploma.diplomaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
